acfbb6bd7d3b7672a9939a9309aa37dba21d27c1
Darryl Dieckman <darryl.dieckman@gmail.com>
2012-03-13 07:29:17 -0700

    Added a quick controller that does not much

---
+++ src/main/java/com/placecruncher/server/controller/SourceController.java
@@ -0,0 +1,26 @@
+package com.placecruncher.server.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.placecruncher.server.dao.SourceDao;
+import com.placecruncher.server.domain.Source;
+
+@Controller
+@RequestMapping("/sources")
+public class SourceController {
+
+    @Autowired
+    private SourceDao sourceDao;
+
+    @RequestMapping(value = "")
+    public ModelAndView listSources(@RequestParam("status") Source.StatusEnum status) {
+        ModelAndView mav = new ModelAndView("sources");
+        mav.addObject("sources", sourceDao.findByStatus(status));
+        return mav;
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/sources.jsp
@@ -0,0 +1,26 @@
+<%@ include file="../taglibs.inc"%>
+<tiles:insertDefinition name="default" flush="true">
+    <tiles:putAttribute name="title" value="PlaceCruncher" />
+    <tiles:putAttribute name="body" type="string">
+
+<table width="100%" style="border: 1px solid black;">
+  <thead>
+    <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Status</th>
+        <th>URL</th>
+    </tr>
+  </thead>
+  <tbody>
+      <c:forEach var="source" items="${sources}">
+        <tr >
+          <td>${source.id}</td>
+          <td>${source.status}</td>
+          <td>${source.url}</td>
+        </tr>
+      </c:forEach>
+  </tbody>
+</table>
+    </tiles:putAttribute>
+</tiles:insertDefinition>
