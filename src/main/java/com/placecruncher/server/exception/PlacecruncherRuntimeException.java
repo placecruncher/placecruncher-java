67931bfbb16fce3881255d2235cbae60af8438b9
Darryl Dieckman <darryl.dieckman@gmail.com>
2012-08-25 07:12:24 -0700

    Added the Swagger API client Added some Jaxb stuff to generate classes for a WADL library Added a SwaggerController which might someday generate dynamic Swagger specifications Added a WADL controller which might someday generate dynamic WADL specifications

--- src/main/java/com/placecruncher/server/exception/PlacecruncherRuntimeException.java
+++ src/main/java/com/placecruncher/server/exception/PlacecruncherRuntimeException.java
@@ -1,14 +1,10 @@
-// Copyright 2011 INQ Mobile. All rights reserved.
 package com.placecruncher.server.exception;
 
 
-/**
- * Default runtime exception for cloud.
- */
 public class PlacecruncherRuntimeException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
-    
+
     public PlacecruncherRuntimeException(String msg, Throwable cause) {
         super(msg, cause);
     }
